cffad7330d0aa392a5e22ddb266352105fabd1a6
James Allan <james.allan@automattic.com>
2021-08-19 16:06:51 +1000

    Add the WC_Payments_Subscription_Change_Payment_Method class

--- includes/subscriptions/class-wc-payments-subscriptions.php
+++ includes/subscriptions/class-wc-payments-subscriptions.php
@@ -52,10 +52,13 @@ class WC_Payments_Subscriptions {
 		include_once __DIR__ . '/class-wc-payments-product-service.php';
 		include_once __DIR__ . '/class-wc-payments-invoice-service.php';
 		include_once __DIR__ . '/class-wc-payments-subscription-service.php';
+		include_once __DIR__ . '/class-wc-payments-subscription-change-payment-method-handler.php';
 
 		self::$product_service      = new WC_Payments_Product_Service( $api_client );
 		self::$tax_service          = null; // TODO: create instance of tax service once implemented.
 		self::$invoice_service      = new WC_Payments_Invoice_Service( $api_client, self::$tax_service );
 		self::$subscription_service = new WC_Payments_Subscription_Service( $api_client, $customer_service, self::$tax_service, self::$product_service, self::$invoice_service );
+
+		new WC_Payments_Subscription_Change_Payment_Method_Handler();
 	}
 }
